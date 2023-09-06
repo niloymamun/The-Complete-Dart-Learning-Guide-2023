cdbff6814b243517a73158bbf768c718e365ec4d
AL MAMUN <143861941+niloymamun@users.noreply.github.com>
2023-09-06 19:14:39 +0600

    Update 18 Exception.dart

--- my_dart_course/lib/18 Exception.dart
+++ my_dart_course/lib/18 Exception.dart
@@ -5,17 +5,25 @@ In Dart, an exception is an object that represents an abnormal
    conditions gracefully, allowing you to control 
    how your program responds when something goes wrong.
 */
-void main(){
-while(true){
-// Use try catch find error 
-  try{
-    stdout.write("Enter Your Birthday");
-    var birthday  = num.parse(stdin.readLineSync());
-  var age = DateTime.now().year - birthday;
-  print("Your Age is $age Years Old");
-   break;
-  } catch (exception){// any error than work catch and print exception
-    print(exception);
+import 'dart:io';
+//Exception
+
+void main() {
+  while (true) {
+    try {
+      print("Enter Your Birthday:");
+      var birthdayYear = num.parse(stdin.readLineSync()!);
+      var age = DateTime.now().year - birthdayYear;
+      print("Your age is $age Years old");
+      break;
+    } on FormatException {
+      print('Invalid Number ');
+
+    }catch (value) {
+      print("Error $value");
+    }
+  }
+  }
   }
 }
 }
