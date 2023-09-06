fd06ca9230c8b28f08f5f55cde71ecaaa92b4d5a
AL MAMUN <143861941+niloymamun@users.noreply.github.com>
2023-09-06 19:12:35 +0600

    Create 20 List.dart
    Dart List update methods

---
+++ my_dart_course/lib/20 List.dart
@@ -0,0 +1,34 @@
+void main() {
+  // Create a list
+  var list1 = [0, 4, 5, 6, 7, 8, 9];
+  
+
+  // Print List1 output Result  [4, 5, 6, 7, 8, 9],
+  print(list1);
+
+// Print only index 0 position. outupt Result  4
+  print(list1[3]);
+  // List value first index
+  print(list1.first);
+  // List value last index
+  print(list1.last);
+  // List value reversed
+  print(list1.reversed);
+  // Change  value index 0 position
+  list1[0] = 1;
+  print(list1);
+
+  /*
+   Output Console 
+    [0, 4, 5, 6, 7, 8, 9]
+6
+0
+9
+(9, 8, 7, 6, 5, 4, 0)
+[1, 4, 5, 6, 7, 8, 9]
+   */
+  
+  // Dart Common Used Methods 
+  list1.add(20);
+  
+}
