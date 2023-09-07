82990398c884957553afc650c3c86aacf33658e7
AL MAMUN <143861941+niloymamun@users.noreply.github.com>
2023-09-07 13:29:16 +0600

    Create 23 Sets.dart

---
+++ my_dart_course/lib/23 Sets.dart
@@ -0,0 +1,16 @@
+void main(){
+  // Sets Details
+  var x = {4,5,6};
+  Set y = {7,8,9};
+  x.forEach((value){
+    
+    print(value);
+  });
+  
+  print(y);
+  // Convert set to list 
+  List z = x.toList();
+  print(z);
+  
+  
+}
