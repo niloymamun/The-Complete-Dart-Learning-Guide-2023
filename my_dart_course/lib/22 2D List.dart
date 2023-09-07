55923b63a0ef7660eafe4eff0acc4d07d1ade380
AL MAMUN <143861941+niloymamun@users.noreply.github.com>
2023-09-07 13:13:40 +0600

    Create 22 2D List.dart

---
+++ my_dart_course/lib/22 2D List.dart
@@ -0,0 +1,19 @@
+void main() {
+  // This is 2D list in dart
+  var a = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+  //print 2D list Example
+
+  for (var i = 0; i < a.length; i++) {
+    print(a[i]);
+  }
+  //output console
+
+/* [1, 2, 3]
+[4, 5, 6]
+[7, 8, 9]
+  */
+}
