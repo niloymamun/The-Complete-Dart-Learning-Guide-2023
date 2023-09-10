269b2aa60f31b0056c516ee5597be269356859f2
AL MAMUN <143861941+niloymamun@users.noreply.github.com>
2023-09-10 20:17:38 +0600

    Update

--- my_dart_course/lib/34 Map Function.dart
+++ my_dart_course/lib/34 Map Function.dart
@@ -1,6 +1,22 @@
 void main(){
   var list1 = [1,2,3,4,5];
-  var list2=[list1,6,7,8,...[9,10]];
+  var list2=[list1,6,7,8,[9,10]];
   print(list2);
+  // Some methods 
+  var list =[10,20,30,40,50,3];
+  print(list.any((value)=>value%2==0));
+  print(list.every((value)=>value%2==0));
+  print(list.take(4));
+  int sum=0;
+  
+  List<List<int>> mylist=[[10,20,30,40,40]];
+  var result = mylist[0].map((value){
+    
+    sum +=value;
+    return value;
+    
+  }).toList();
+  print("Sum = $sum");
+  print("Result =$result");
   
 }
